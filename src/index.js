75ed4ac55d6f6f7856c52275fec19ab0323d8583
Giu Magnani <glmagnani@gmail.com>
2019-01-11 01:13:37 +0100

    Add image. Progress

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import character from "./assets/char-sprite.png";
 import ground from "./assets/platform.png";
 import bomb from "./assets/bomb.png";
+import star from "./assets/star.png";
 
 const config = {
   type: Phaser.AUTO,
@@ -25,9 +26,10 @@ const config = {
 
 let player;
 let platforms;
+let stars;
 let cursors;
 let objects;
-let food = 0;
+let resources = 0;
 let scoreText;
 let speed = 160;
 const game = new Phaser.Game(config);
@@ -35,12 +37,14 @@ const game = new Phaser.Game(config);
 function preload() {
   this.load.image("ground", ground);
   this.load.image("bomb", bomb);
+  this.load.image("star", star);
   this.load.spritesheet('character', character, { frameWidth: 31, frameHeight: 60 });
 }
 
 function create() {
   platforms = this.physics.add.staticGroup();
   objects = this.physics.add.staticGroup();
+  stars = this.physics.add.staticGroup();
   platforms.create(0, 400, 'ground');
   platforms.create(50, 250, 'ground');
 
@@ -85,12 +89,13 @@ function create() {
     repeat: 0
   });
 
-  scoreText = this.add.text(16, 16, 'Food: 0', { fontSize: '32px', fill: '#fff' });
+  scoreText = this.add.text(16, 16, 'Resources: 0', { fontSize: '32px', fill: '#fff' });
 
   cursors = this.input.keyboard.createCursorKeys();
 
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(objects, platforms);
+  this.physics.add.collider(stars, platforms);
 
   this.physics.add.collider(player, objects, objectReaction, null, this);
 }
@@ -120,20 +125,27 @@ function update() {
     player.setVelocityY(speed);
     player.anims.play('down', true);
   }
+
+  if (cursors.space.isDown && resources >= 20) {
+    resources -= 20;
+    build();
+  }
 }
 
-function build(player, object) {
+function build() {
+  console.log(player);
+  stars.create(player.x, player.y + player.height, 'star');
   // object.disableBody(true, true);
-  // food++;
-  // scoreText.setText('Food: ' + food);
+  // resources++;
+  // scoreText.setText('Resources: ' + resources);
 }
 
 function objectReaction(player, object) {
   if (!object.reacting) {
     object.resources -= 10;
     object.reacting = true;
-    food += 10;
-    scoreText.setText('Food: ' + food);
+    resources += 10;
+    scoreText.setText('Resources: ' + resources);
     if (object.resources <= 0) object.disableBody(true, true);
     setTimeout(() => object.reacting = false, 1000);
   }
